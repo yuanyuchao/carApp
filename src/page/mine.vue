6c04eacd1692c7c352ef375410dd0078d50d6b34
yuanyuchao <499908562@qq.com>
2019-03-28 17:37:41 +0800

    fix

--- src/page/mine.vue
+++ src/page/mine.vue
@@ -1,24 +1,37 @@
 <template>
   <div class="wrapper">
-      <p>账号<input type="text" name="" id=""></p>
-      <p>密码<input type="password" name="" id=""></p>
-      <h1>login</h1>
-
+      <p>账号<input type="text" name="" id="" v-model="Account"></p>
+      <p>密码<input type="password" name="" id="" v-model="Password"></p>
+      <h1 @click="getData">login</h1>
   </div>
 </template>
 
 <script>
-import CryptoJS from 'crypto-js'
+import CryptoJS from 'crypto-js';
+import axios from 'axios';
 export default {
   components:{},
   props:{},
   data(){
     return {
+        Account: '',
+        Password: '',
     }
   },
   watch:{},
   computed:{},
   methods:{
+      getData:function(){
+          axios.post('http://192.168.1.97/Page/LoginCode',{
+             Account:this.Account,
+             Password:this.Password,
+          }).then(res=>{
+              console.log(res);
+          }).catch(err=>{
+               console.log(err)
+          })
+
+      },
        /*加密*/
       encrypt (name, value) {
         let encryptText = CryptoJS.AES.encrypt(value, CryptoJS.enc.Utf8.parse(this.key), {
